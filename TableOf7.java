7d42f10c845ffa461bc6457e93d892092afff214
AjayMorde <103033106+AjayMorde@users.noreply.github.com>
2022-04-08 00:49:43 +0530

    Add files via upload

---
+++ TableOf7.java
@@ -0,0 +1,18 @@
+package welcomeToJava;
+
+public class TableOf7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 1;
+		for( n = 1; n<=10;n=n+1)
+			
+		{
+			 System.out.println(7*n);
+      
+       
+		}
+		
+	}
+
+}
